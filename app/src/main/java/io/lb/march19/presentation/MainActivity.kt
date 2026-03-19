b1f39c2ba879a39ded1c9bdf40149c4d75730173
LeonardoBai12 <69223422+LeonardoBai12@users.noreply.github.com>
2026-03-19 19:02:20 -0300

    Removing unnecessary code

--- app/src/main/java/io/lb/march19/presentation/MainActivity.kt
+++ app/src/main/java/io/lb/march19/presentation/MainActivity.kt
@@ -30,19 +30,3 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    _root_ide_package_.io.lb.march19.presentation.ui.theme.March19Theme {
-        Greeting("Android")
-    }
-}
